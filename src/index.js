f691e8598d3d48dd3e7df60668c091e1c374f1ad
demonikos <119507967+demonikos@users.noreply.github.com>
2023-06-23 20:35:41 +0300

    fixing bugs

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
-import PropTypes from 'prop-types';
-import user from 'DataJSON/user';
+// import PropTypes from 'prop-types';
+// import user from 'DataJSON/user';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
